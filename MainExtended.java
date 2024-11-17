0e46433f491143c1c824d97920ca07752bc05e39
szymzur <szymzur@gmail.com>
2024-11-17 09:07:59 -0400

    Add files via upload

---
+++ MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Szymon";
+        int myAge = 40;
+        double height = 1.81;
+        char carClass = 'S';
+        boolean isMale = true;
+        String myClub = "Athletic";
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isMale);
+        System.out.println(myClub);
+    }
+}

075f91e198bc37c7ac65866918000217a92753f9
Szymon Zurek <szymzur@gmail.com>
2024-12-13 16:33:21 +0100

    Task 1

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {         // [2]
-        System.out.println("Hello World!");
-        System.out.println("My name is Szymon");
-        System.out.println("My age is " + 40);
-        System.out.println('S');
-    }
-    }

c2558640d82ee32d248ba3e9d986c520c412511c
Szymon Zurek <szymzur@gmail.com>
2024-12-20 16:13:46 +0100

    TestSuite

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -14,4 +14,21 @@ public class CalculatorTestSuite {
         int sumResult = calculator.sum(a, b);
         assertEquals(13, sumResult);
     }
+
+    @Test
+    public void testSub() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int subResult = calculator.sub(a, b);
+        assertEquals(-3, subResult);
+    }
+
+    @Test
+    public void testSquare() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        double squareResult = calculator.square(a);
+        assertEquals(25, squareResult, 1e-9);
+    }
 }

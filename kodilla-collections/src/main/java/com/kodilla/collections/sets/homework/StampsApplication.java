bcf984f63bb63f2bbe8dd55572b998ba3ce5c915
Szymon Zurek <szymzur@gmail.com>
2025-01-03 18:20:35 +0100

    Stamps 4.4

--- kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/StampsApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/StampsApplication.java
@@ -7,15 +7,10 @@ public class StampsApplication {
     public static void main(String[] args) {
         Set<Stamp> stamps = new HashSet<>();
 
-        Stamp stamp1 = new Stamp("Butterfly", 3.0, 2.5, true);
-        Stamp stamp2 = new Stamp("Bird", 4.0, 3.0, false);
-        Stamp stamp3 = new Stamp("Flower", 2.5, 2.5, true);
-        Stamp stamp4 = new Stamp("Butterfly", 3.0, 2.5, true);
-
-        stamps.add(stamp1);
-        stamps.add(stamp2);
-        stamps.add(stamp3);
-        stamps.add(stamp4);
+        stamps.add(new Stamp("Butterfly", 3.0, 2.5, true));
+        stamps.add(new Stamp("Bird", 4.0, 3.0, false));
+        stamps.add(new Stamp("Flower", 2.5, 2.5, true));
+        stamps.add(new Stamp("Butterfly", 3.0, 2.5, true));
 
         System.out.println("Stamps collection size: " + stamps.size());
         for (Stamp stamp : stamps) {

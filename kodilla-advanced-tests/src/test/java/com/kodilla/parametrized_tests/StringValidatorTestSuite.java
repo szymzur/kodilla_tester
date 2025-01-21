f61d0dfa1010a591adbfe85364f3bc43292f1435
Szymon Zurek <szymzur@gmail.com>
2025-01-21 19:09:03 +0100

    7.0

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringValidatorTestSuite.java
@@ -0,0 +1,47 @@
+package com.kodilla.parametrized_tests;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringValidatorTestSuite {
+
+    private StringValidator validator = new StringValidator();
+
+    @Test
+    public void shouldReturnFalseIfStringIsNotEmpty() {
+        assertFalse(validator.isBlank("test"));
+    }
+
+//    @Test
+//    public void shouldReturnTrueIfStringIsNotEmpty () {
+//        assertTrue(validator.isBlank(""));
+//    }
+//
+//    @Test
+//    public void shouldReturTrueIfStringHasOnlySpaces() {
+//        assertTrue(validator.isBlank("    "));
+//    }
+
+//    @ParameterizedTest
+//    @ValueSource(strings = {"", "   "})
+//    public void shouldReturnTrueIfStringIsEmpty(String text) {
+//        assertTrue(validator.isBlank(text));
+//    }
+
+    @Test
+    public void shouldReturnTrueIfStringIsNull() {
+        assertTrue(validator.isBlank(null));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void shouldReturnTrueIfStringIsEmpty(String text) {
+        System.out.println("|" + text + "|");
+        assertTrue(validator.isBlank(text));
+    }
+}

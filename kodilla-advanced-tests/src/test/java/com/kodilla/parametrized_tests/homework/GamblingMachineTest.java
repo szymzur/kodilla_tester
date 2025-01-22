d999022e300b890adee27d9212af1f0fa41a34fa
Szymon Zurek <szymzur@gmail.com>
2025-01-22 21:06:04 +0100

    7.2.1 Homework

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTest.java
@@ -3,7 +3,6 @@ package com.kodilla.parametrized_tests.homework;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -11,15 +10,30 @@ import java.util.stream.Stream;
 public class GamblingMachineTest {
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/testdata.csv", numLinesToSkip = 1)
-    public void testHowManyWinsWithCsvFile(String csvRow, int expectedWinCount) throws InvalidNumbersException {
+    @CsvFileSource(resources = "/testdata.csv")
+    public void testInvalidSize(String csvRow) {
         Set<Integer> userNumbers = Stream.of(csvRow.split(","))
                 .map(Integer::parseInt)
                 .collect(Collectors.toSet());
 
-        GamblingMachine machine = new GamblingMachine();
-        int winCount = machine.howManyWins(userNumbers);
+        Set<Integer> invalidSizeNumbers = userNumbers.stream().limit(3).collect(Collectors.toSet());
 
-        assertEquals(expectedWinCount, winCount);
+        GamblingMachine machine = new GamblingMachine();
+
+        assertThrows(InvalidNumbersException.class, () -> machine.howManyWins(invalidSizeNumbers));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testdata.csv")
+    public void testOutOfScopeNumbers(String csvRow) {
+        Set<Integer> userNumbers = Stream.of(csvRow.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toSet());
+
+        Set<Integer> outOfScopeNumbers = userNumbers.stream().map(num -> num + 50).collect(Collectors.toSet());
+
+        GamblingMachine machine = new GamblingMachine();
+
+        assertThrows(InvalidNumbersException.class, () -> machine.howManyWins(outOfScopeNumbers));
     }
 }

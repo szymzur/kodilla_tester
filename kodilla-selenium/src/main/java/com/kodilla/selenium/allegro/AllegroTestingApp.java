e085d5ba2a4dd2e345e78823f25bc26d2878c8d9
Szymon Zurek <szymzur@gmail.com>
2025-03-08 18:09:06 +0100

    13.5 selektory css

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.Alert;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.util.List;
 
 public class AllegroTestingApp {
     public static void main(String[] args) {
@@ -15,27 +18,26 @@ public class AllegroTestingApp {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("==remote-allow-origins=*");
         WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("https://allegro.pl/");
-
-        // Zamknięcie wyskakującego okienka (alert)
-        try {
-            Alert alert = driver.switchTo().alert();
-            alert.accept();
-        } catch (Exception e) {
-            System.out.println("Brak wyskakującego okienka.");
-        }
-
-        // Znajdź i wybierz kategorię "Elektronika" używając XPath Relative
-        WebElement categoryCombo = driver.findElement(By.xpath("//select[contains(@id, 'category')]"));
-        Select categorySelect = new Select(categoryCombo);
-        categorySelect.selectByVisibleText("Elektronika");
+        driver.get("https://www.ebay.pl/");
 
         // Wprowadź "Mavic mini" do pola wyszukiwania
-        WebElement searchBox = driver.findElement(By.xpath("//input[contains(@type, 'search')]"));
+        WebElement searchBox = driver.findElement(By.cssSelector("input[type='text']"));
         searchBox.sendKeys("Mavic mini");
 
-        // Kliknij przycisk "Szukaj" używając XPath Relative
-        WebElement searchButton = driver.findElement(By.xpath("//button[contains(@type, 'submit')]"));
+        // Kliknij przycisk "Szukaj" używając className
+        WebElement searchButton = driver.findElement(By.className("gh-search-button__label"));
         searchButton.click();
+
+        // Dodaj wait, który poczeka na pierwszy wynik wyszukiwania
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".s-item")));
+
+        // Znajdź wszystkie karty produktów i przypisz je do jednej listy
+        List<WebElement> productCards = driver.findElements(By.cssSelector(".s-item"));
+
+        // Wyświetl informacje o produkcie za pomocą getText()
+        for (WebElement productCard : productCards) {
+            System.out.println(productCard.getText());
+        }
     }
 }

f7be1a565c32b68ca22586e3eb8d922c3918369b
Szymon Zurek <szymzur@gmail.com>
2025-03-13 20:01:41 +0100

    14.3

---
+++ kodilla-cucumber/src/test/java/wallet/TestRunner.java
@@ -0,0 +1,10 @@
+package wallet;
+
+import org.junit.platform.suite.api.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("/")
+public class TestRunner {
+
+}
---
+++ kodilla-cucumber/src/test/java/wallet/WalletSteps.java
@@ -0,0 +1,28 @@
+package wallet;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions; // Importuj tylko Assertions z JUnit 5
+
+public class WalletSteps {
+    private final Wallet wallet = new Wallet();
+    private final CashSlot cashSlot = new CashSlot();
+
+    @Given("I have deposited $200 in my wallet")
+    public void i_have_deposited_$200_in_my_wallet() {
+        wallet.deposit(200);
+        Assertions.assertEquals(200, wallet.getBalance(), "Incorrect wallet balance");
+    }
+
+    @When("I request $30")
+    public void i_request_$30() {
+        Cashier cashier = new Cashier(cashSlot);
+        cashier.withdraw(wallet, 30);
+    }
+
+    @Then("$30 should be dispensed")
+    public void $30_should_be_dispensed() {
+        Assertions.assertEquals(30, cashSlot.getContents());
+    }
+}

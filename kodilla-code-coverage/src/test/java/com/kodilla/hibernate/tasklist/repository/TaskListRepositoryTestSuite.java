d0be4b7adb657ae587596ff5aab3e2022b77f35e
Szymon Zurek <szymzur@gmail.com>
2025-03-22 10:24:31 +0100

    15.4.4

--- kodilla-code-coverage/src/test/java/com/kodilla/hibernate/tasklist/repository/TaskListRepositoryTestSuite.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/hibernate/tasklist/repository/TaskListRepositoryTestSuite.java
@@ -1,6 +1,6 @@
 package com.kodilla.hibernate.tasklist.repository;
 
-import com.kodilla.CodeCoverageApplication; // Import klasy głównej aplikacji
+import com.kodilla.CodeCoverageApplication;
 import com.kodilla.hibernate.tasklist.TaskList;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,29 +9,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-@SpringBootTest(classes = CodeCoverageApplication.class) // Wskazanie klasy głównej aplikacji
+@SpringBootTest(classes = CodeCoverageApplication.class)
 class TaskListRepositoryTestSuite {
 
     @Autowired
-    private TaskListRepository taskListRepository; // Repozytorium z automatycznym wstrzykiwaniem zależności
+    private TaskListRepository taskListRepository;
 
     @Test
     void testFindByListName() {
         // Given
         TaskList taskList = new TaskList("Test List", "This is a test description");
-        taskListRepository.save(taskList); // Zapisujemy obiekt TaskList w bazie danych
+        taskListRepository.save(taskList);
         String listName = taskList.getListName();
 
         // When
         List<TaskList> result = taskListRepository.findByListName(listName);
 
         // Then
-        Assertions.assertEquals(1, result.size()); // Upewniamy się, że znaleziono dokładnie jeden wynik
-        Assertions.assertEquals("Test List", result.get(0).getListName()); // Weryfikujemy nazwę listy
-        Assertions.assertEquals("This is a test description", result.get(0).getDescription()); // Weryfikujemy opis
+        Assertions.assertEquals(1, result.size());
+        Assertions.assertEquals("Test List", result.get(0).getListName());
+        Assertions.assertEquals("This is a test description", result.get(0).getDescription());
 
         // CleanUp
         int id = result.get(0).getId();
-        taskListRepository.deleteById(id); // Usuwamy dane z bazy
+        taskListRepository.deleteById(id);
     }
 }
